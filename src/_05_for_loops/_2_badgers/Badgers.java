c9958909a02835bc418f3ea86941245d4b36b0c6
root <root@696e09f9e2e1>
2022-07-10 19:57:35 +0000

    only finished badgers

--- src/_05_for_loops/_2_badgers/Badgers.java
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -1,20 +1,36 @@
 package _05_for_loops._2_badgers;
 
 public class Badgers {
+public static void main(String[] args) {
+	int for1 ;
+	int for2 ;
+	for (for1=0; for1 < 12 ; for1 +=1 ) {
+	System.out.println("Badger");
+	}
+	for (for2=0; for2 < 2 ; for2 +=1 )  {
+		System.out.println("Mushroom");
+	}
+	System.out.println("Snake");
+}
 
 	public Badgers() {
 		// TODO Auto-generated constructor stub
-	}
 	
-	
-}
- for (mushroom=1; mushroom <= 1; ) {
-	 System.out.println(mushroom);
- }
- for (badger=1; badger <= 1; badger+=1 ) {
-	 System.out.println(Badger);
- }
+			
 		
+		
+		
+		
+			
+	}
+	 
+	
+	
+
+}
+ 
+ 
+	
 		
 		
 	 
